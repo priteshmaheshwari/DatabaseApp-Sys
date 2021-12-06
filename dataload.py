13d797c5e09fef70dcbba37fff99f1eeb2dcb800
Varad0119 <78159099+Varad0119@users.noreply.github.com>
2021-12-05 20:37:43 -0500

    Add files via upload

---
+++ dataload.py
@@ -0,0 +1,33 @@
+import io
+import csv
+import math
+import psycopg2
+import numpy as np
+import pandas as pd
+import json as simplejson
+from pymongo import MongoClient
+from sqlalchemy import create_engine
+#import database
+
+
+		
+engine = create_engine('postgresql+psycopg2://nyc_covid:nyc_covid@localhost:5432/nyc_covid')
+
+hospital_df= pd.read_csv('./Final_Data/hospital_data.csv')
+hospital_df.to_sql('hospital_data', engine, if_exists='append', index=False)
+
+metadata_df= pd.read_csv('./Final_Data/metadata.csv')
+metadata_df.to_sql('metadata', engine, if_exists='append', index=False)
+
+contact_df = pd.read_csv('./Final_Data/contact_data.csv')
+contact_df.to_sql('contact_data', engine, if_exists='append', index=False)
+
+address_df = pd.read_csv('./Final_Data/address_data.csv')
+address_df.to_sql('address_data', engine, if_exists='append', index=False)
+
+cases_df = pd.read_csv('./Final_Data/us-counties-2021.csv')
+cases_df.to_sql('counties', engine, if_exists='append', index=False)
+
+
+print('data loaded successfully')
+						
